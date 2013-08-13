095a81f6f8d1c64417c75f12f1410b255df62bea
Chris Hapgood <cch1@hapgoods.com>
2013-08-13 11:30:50 -0400

    Remove crufty dependencies
    Signed-off-by: Chris Hapgood <cch1@hapgoods.com>

--- project.clj
+++ project.clj
@@ -9,9 +9,6 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.4"]
-                 [org.jsoup/jsoup "1.7.2"]
-                 [org.clojars.kyleburton/clj-xpath "1.4.0"]
-                 [xalan "2.7.1"] ;; Direct dependency for roomkey.xml.parsers
                  [com.netflix.curator/curator-framework "1.3.3"]
                  [com.netflix.curator/curator-recipes "1.3.3"]]
   :repositories [["releases" {:url "s3p://rk-maven/releases/"}]]
