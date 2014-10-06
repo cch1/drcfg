97d1e296aea0ff5a7a3aa19130591d1b8659d38b
Vincent Rivellino <vrivellino@roomkey.com>
2014-10-06 12:57:09 -0400

    Switched from s3-wagon-private to lein-maven-s3-wagon

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Dynamic Runtime Configuration Utility based on Zookeeper"
   :url "https://github.com/g1nn13/drcfg"
   :plugins [[com.roomkey/lein-v "4.0.2"]
-            [s3-wagon-private "1.1.2"]]
+            [lein-maven-s3-wagon "0.2.3"]]
   :license {:name "Copyright Hotel JV Services LLC"
             :distribution :manual
             :comments "All rights reserved"}
@@ -11,7 +11,7 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [com.netflix.curator/curator-framework "1.3.3" :exclusions [org.slf4j/slf4j-log4j12 org.slf4j/slf4j-api log4j]]
                  [com.netflix.curator/curator-recipes "1.3.3" :exclusions [org.slf4j/slf4j-log4j12]]]
-  :repositories [["releases" {:url "s3p://rk-maven/releases/"}]]
+  :repositories [["releases" {:url "s3://rk-maven/releases/"}]]
   :profiles {:dev {:resource-paths ["test-resources"]
                    :dependencies [[midje "1.6.3"]
                                   [org.slf4j/slf4j-api "1.7.7"]
