6c468dcfe2ebb01a878018dce6c3c595cf55384b
Thomas Steffes <tsteffes@roomkey.com>
2015-01-16 11:02:25 -0500

    Resolving lein ancient
    Signed-off-by: Thomas Steffes <tsteffes@roomkey.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.roomkey/drcfg :lein-v
   :description "Dynamic Runtime Configuration Utility based on Zookeeper"
   :url "https://github.com/g1nn13/drcfg"
-  :plugins [[com.roomkey/lein-v "4.0.2"]
-            [lein-maven-s3-wagon "0.2.3"]]
+  :plugins [[com.roomkey/lein-v "4.1.0"]
+            [lein-maven-s3-wagon "0.2.4"]]
   :license {:name "Copyright Hotel JV Services LLC"
             :distribution :manual
             :comments "All rights reserved"}
@@ -14,7 +14,7 @@
   :repositories [["rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}] ["releases" {:url "s3://rk-maven/releases/"}]]
   :profiles {:dev {:resource-paths ["test-resources"]
                    :dependencies [[midje "1.6.3"]
-                                  [org.slf4j/slf4j-api "1.7.7"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.7"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.7"]
+                                  [org.slf4j/slf4j-api "1.7.10"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.10"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.10"]
                                   [log4j/log4j "1.2.17"]]}})
