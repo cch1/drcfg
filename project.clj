4cd4d6f9f9c41b07223a5694917ad216e5ec4c43
Chris Hapgood <cch1@hapgoods.com>
2015-05-04 16:30:37 -0400

    Update dev logging dependencies

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :repositories [["rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}] ["releases" {:url "s3://rk-maven/releases/"}]]
   :profiles {:dev {:resource-paths ["test-resources"]
                    :dependencies [[midje "1.6.3"]
-                                  [org.slf4j/slf4j-api "1.7.10"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.10"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.10"]
+                                  [org.slf4j/slf4j-api "1.7.12"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.12"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.12"]
                                   [log4j/log4j "1.2.17"]]}})
