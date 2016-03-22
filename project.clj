fc2cd0ea93a1a104a5f0d168098747954b4b2541
Chris Hapgood <cch1@hapgoods.com>
2016-03-22 13:55:44 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.roomkey/drcfg :lein-v
   :description "Dynamic Runtime Configuration Utility based on Zookeeper"
   :url "https://github.com/g1nn13/drcfg"
-  :plugins [[com.roomkey/lein-v "5.0.0"]]
+  :plugins [[com.roomkey/lein-v "5.0.1"]]
   :license {:name "Copyright Hotel JV Services LLC"
             :distribution :manual
             :comments "All rights reserved"}
@@ -9,17 +9,17 @@
                   ["v" "update"] ;; compute new version & tag it
                   ["vcs" "push"]
                   ["deploy"]]
-  :min-lein-version "2.2.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :min-lein-version "2.5.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.apache.curator/curator-framework "2.8.0"]
-                 [org.apache.curator/curator-recipes "2.8.0"]]
+                 [org.apache.curator/curator-framework "3.1.0"]
+                 [org.apache.curator/curator-recipes "3.1.0"]]
   :repositories [["rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}]
                  ["releases" {:url "s3://rk-maven/releases/"}]]
   :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[midje "1.7.0"]
-                                  [org.apache.curator/curator-test "2.8.0"]
-                                  [org.slf4j/slf4j-api "1.7.12"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.12"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.12"]
+                   :dependencies [[midje "1.8.3"]
+                                  [org.apache.curator/curator-test "3.1.0"]
+                                  [org.slf4j/slf4j-api "1.7.19"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.19"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.19"]
                                   [log4j/log4j "1.2.17"]]}})
