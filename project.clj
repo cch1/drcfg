03832bb8d1ad63b9f472ed697c1adf8d80cbd0b1
Chris Hapgood <cch1@hapgoods.com>
2016-04-04 16:51:54 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -12,16 +12,16 @@
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [zookeeper-clj "0.9.1" :exclusions [org.apache.zookeeper/zookeeper]]
+                 [zookeeper-clj "0.9.4" :exclusions [org.apache.zookeeper/zookeeper]]
                  [org.apache.curator/curator-framework "3.1.0"]
                  [org.apache.curator/curator-recipes "3.1.0"]]
   :repositories [["rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}]
                  ["releases" {:url "s3://rk-maven/releases/"}]]
   :jvm-opts ["-Djava.io.tmpdir=./tmp"]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]
+  :profiles {:dev {:dependencies [[midje "1.8.3" :exclusions [commons-codec]]
                                   [org.apache.curator/curator-test "3.1.0"]
-                                  [org.slf4j/slf4j-api "1.7.19"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.19"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.19"]
+                                  [org.slf4j/slf4j-api "1.7.21"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.21"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.21"]
                                   [log4j/log4j "1.2.17"]]}
              :test {:resource-paths ["test-resources"]}})
