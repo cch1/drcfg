230740bab35e8251bfa8d8ccda1c2359894fd675
Chris Hapgood <cch1@hapgoods.com>
2016-04-08 15:39:56 -0400

    Remove roomkey.zkutil & roomkey.drcfg.client, remove curator dependency

--- project.clj
+++ project.clj
@@ -12,9 +12,7 @@
   :min-lein-version "2.5.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [zookeeper-clj "0.9.4" :exclusions [org.apache.zookeeper/zookeeper]]
-                 [org.apache.curator/curator-framework "3.1.0"]
-                 [org.apache.curator/curator-recipes "3.1.0"]]
+                 [zookeeper-clj "0.9.4" :exclusions [org.apache.zookeeper/zookeeper]]]
   :repositories [["rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}]
                  ["releases" {:url "s3://rk-maven/releases/"}]]
   :jvm-opts ["-Djava.io.tmpdir=./tmp"]
