287f136342dbc1362b80687f0a5f4dfaa56a11f9
Chris Hapgood <cch1@hapgoods.com>
2016-07-28 23:12:57 -0400

    Make examples in user.clj more realistic

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -9,20 +9,23 @@
             [clojure.core.async :as async]
             [clojure.string :as string]))
 
-(def $connect-string "localhost:2181/drcfg")
+(def $connect-string "localhost:2181")
 (def $path "/apij.controllers.interstitial.rk-int-alg/template-filters")
 
 ;; Setup tunnels to dev zookeeper cluster first or this will throw an exception
 (defn ex0 []
-  (let [z (zref/zref $path {})]
-    (with-open [c (zoo/connect $connect-string)]
-      (.zConnect (.zPair z c))
-;      (>break!)
-      )))
+  (let [connect-string (str $connect-string "/drcfg")
+        z (zref/zref $path {})
+        r (zref/zref "/" nil)]
+    (with-open [c (zoo/connect connect-string)]
+      (.zConnect (.zInitialize (.zPair z c)))
+      (.zConnect (.zInitialize (.zPair r c)))
+      (comment >break!))))
 
 ;; Upgrade to resilient zclient with go-loop monitoring its events and updating zref
 (defn ex1 []
-  (let [z (zref/zref $path {})
+  (let [connect-string (str $connect-string "/drcfg")
+        z (zref/zref $path {})
         ch (async/chan 1)]
     (async/go-loop [state {:paired? false :intialized? false}]
       (if-let [[ev client :as m] (async/<! ch)]
@@ -42,6 +45,13 @@
         (do ;; client input channel has closed, we're outta here
           (.zDisconnect z)
           (log/debugf "Client input channel has closed for %s, shutting down" (.path z)))))
-    (with-open [c (zclient/create $connect-string ch)]
- ;     (>break!)
-      )))
+    (with-open [c (zclient/create connect-string ch)]
+      (comment >break!))))
+
+;; Use public drcfg vars
+(defn ex2 []
+  (binding [roomkey.drcfg/*registry* (atom #{})]
+    (let [[r rm] (>- "/" nil)
+          [z zm] (>- $path {})]
+      (with-open [c (open @roomkey.drcfg/*registry* $connect-string)]
+        (comment >break!)))))
