6ad60bc44f88c1f85e161d54f13f6d088d6ab4e2
Chris Hapgood <cch1@hapgoods.com>
2018-04-03 15:25:44 -0400

    Switch to secure repositories

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
   :release-tasks [["vcs" "assert-committed"]
                   ["v" "update"] ;; compute new version & tag it
                   ["vcs" "push"]
-                  ["deploy"]]
-  :min-lein-version "2.5.0"
+                  ["deploy" "rk-maven"]]
+  :min-lein-version "2.8.1"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/core.async "0.4.474"]
                  [zookeeper-clj "0.9.4" :exclusions [org.apache.zookeeper/zookeeper]]
                  [org.apache.zookeeper/zookeeper "3.5.3-beta"]]
-  :repositories [["releases" {:url "s3://rk-maven/releases/"}]]
+  :repositories {"rk-maven" {:url "s3p://rk-maven/releases/" :no-auth true}}
   :jvm-opts ["-Djava.io.tmpdir=./tmp"]
   :profiles {:dev {:dependencies [[midje "1.9.1" :exclusions [commons-codec]]
                                   [org.apache.curator/curator-test "4.0.1"]
