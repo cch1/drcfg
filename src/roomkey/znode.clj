c70fdd037f4b0c0cd5be2d75f4ee0c432ee92122
Chris Hapgood <cch1@hapgoods.com>
2019-01-22 00:53:58 -0500

    Be consistent if not fast

--- src/roomkey/znode.clj
+++ src/roomkey/znode.clj
@@ -259,7 +259,7 @@
   (close! [this] (impl/close! events))
 
   java.lang.Object
-  (equals [this other] (and (= (class this) (class other)) (= path (.path other)) (= (.client this) (.client other))))
+  (equals [this other] (and (= (class this) (class other)) (= (.path this) (.path other)) (= (.client this) (.client other))))
   (hashCode [this] (.hashCode [path client]))
   (toString [this] (format "%s: %s" (.. this (getClass) (getSimpleName)) path)))
 
