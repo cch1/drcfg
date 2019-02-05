f6445c4329143bd711d986964d1dada6587cc816
Chris Hapgood <cch1@hapgoods.com>
2019-02-05 16:50:31 -0500

    Add server-facing integration tests

---
+++ test/roomkey/integration/start_stop_zclient.clj
@@ -0,0 +1,11 @@
+(ns roomkey.integration.start-stop-zclient
+  (:require [roomkey.zclient :refer :all]
+            [midje.sweet :refer :all]))
+
+(def connect-string "zk1.c0pt3r.local,zk2.c0pt3r.local,zk3.c0pt3r.local")
+
+(fact "Can create a client, open it and then close it repeatedly"
+      (let [$c (create)]
+        (for [n (range 5)]
+          (let [handle (open $c (str connect-string "/drcfg") 8000)]
+            (.close handle))) => (five-of true)))
---
+++ test/roomkey/integration/start_stop_znode.clj
@@ -0,0 +1,15 @@
+(ns roomkey.integration.start-stop-znode
+  (:require [roomkey.znode :refer :all]
+            [clojure.core.async :as async]
+            [clojure.tools.logging :as log]
+            [midje.sweet :refer :all]
+            [midje.checking.core :refer [extended-=]]))
+
+(def connect-string "zk1.c0pt3r.local,zk2.c0pt3r.local,zk3.c0pt3r.local")
+
+(fact "Can create a client, open it and then close it repeatedly"
+      (let [$z (new-root)]
+        (for [n (range 6)]
+          (let [handle (open $z (str connect-string "/drcfg") 8000)]
+            (Thread/sleep 100)
+            (.close handle))) => (six-of int?)))
