34b96fbcb965602ad6618abff37dc642ed0b72d4
Chris Hapgood <cch1@hapgoods.com>
2019-04-09 17:47:30 -0400

    Update midje test dependency

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  [org.apache.zookeeper/zookeeper "3.5.4-beta" :exclusions [log4j]]]
   :repositories {"rk-maven" {:url "s3p://rk-maven/releases/" :no-auth true}}
   :jvm-opts ["-Djava.io.tmpdir=./tmp"]
-  :profiles {:dev {:dependencies [[midje "1.9.6"]
+  :profiles {:dev {:dependencies [[midje "1.9.8"]
                                   [zookeeper-clj "0.9.4" :exclusions [org.apache.zookeeper/zookeeper commons-codec]]
                                   [org.apache.curator/curator-test "4.2.0"]
                                   [org.slf4j/slf4j-api "1.7.26"]
