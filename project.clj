24045e788045493f8b193e65c227bc6cd9c1c174
Chris Hapgood <cch1@hapgoods.com>
2019-08-09 11:10:15 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -13,19 +13,19 @@
                   ["v" "abort-when-not-anchored"]
                   ["deploy" "rk-maven"]]
   :min-lein-version "2.8.1"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [org.clojure/core.async "0.4.490"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [org.clojure/core.async "0.4.500"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [org.apache.zookeeper/zookeeper "3.5.4-beta" :exclusions [log4j]]]
+                 [org.apache.zookeeper/zookeeper "3.5.5"]]
   :repositories {"rk-maven" {:url "s3p://rk-maven/releases/" :no-auth true}}
   :jvm-opts ["-Djava.io.tmpdir=./tmp"]
-  :profiles {:dev {:dependencies [[midje "1.9.8"]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]
                                   [zookeeper-clj "0.9.4" :exclusions [org.apache.zookeeper/zookeeper commons-codec]]
                                   [org.apache.curator/curator-test "4.2.0"]
-                                  [org.slf4j/slf4j-api "1.7.26"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.26"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.26"]
+                                  [org.slf4j/slf4j-api "1.7.27"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.27"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.27"]
                                   [log4j/log4j "1.2.17"]]}
              :test {:resource-paths ["test-resources"]
                     :global-vars {*warn-on-reflection* true}}
