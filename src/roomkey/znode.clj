818ec79b6010a8b1df8277c8ac0261628d3c8ff9
Chris Hapgood <cch1@hapgoods.com>
2019-12-21 21:43:58 -0500

    Tweak ZNode channel buffers for consistency and efficiency

--- src/roomkey/znode.clj
+++ src/roomkey/znode.clj
@@ -108,9 +108,9 @@
     (log/tracef "Watching %s" (str this))
     (let [handle-channel-error (fn [e] (log/errorf e "Exception while processing channel event for %s" (str this)))
           znode-events (async/chan 10 identity handle-channel-error)
-          data-events (async/chan (async/sliding-buffer 4) (zdata-xform this) handle-channel-error)
+          data-events (async/chan 1 (zdata-xform this) handle-channel-error)
           delete-events (async/chan 1 (dedupe) handle-channel-error) ; why won't a promise channel work here?
-          children-events (async/chan 10 (tap-children this) handle-channel-error)
+          children-events (async/chan 1 (tap-children this) handle-channel-error)
           exists-events (async/chan 1 (tap-stat this) handle-channel-error)
           handle-connection-loss (make-connection-loss-handler this znode-events)
           watcher (partial async/put! znode-events)]
