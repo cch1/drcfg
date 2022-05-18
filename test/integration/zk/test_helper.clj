93c8d0b9e9c8a2fa15efb3c1b7f54fcbcca73223
Chris Hapgood <cch1@hapgood.com>
2022-05-17 22:17:01 -0400

    Remove midje tests
    All have clojure.test equivalents and were in the old old namespaces.

--- test/integration/zk/test_helper.clj
+++ test/integration/zk/test_helper.clj
@@ -1,14 +1,9 @@
 (ns integration.zk.test-helper
   (:require [clojure.core.async :as async]
-            [clojure.tools.logging :as log]
-            [midje.sweet :refer :all]
-            [midje.checking.core :refer [extended-=]])
+            [clojure.tools.logging :as log])
   (:import [java.time Instant]
            [org.apache.curator.test TestingServer TestingCluster]
            [org.apache.zookeeper ZooKeeper data.Stat]))
-
-(defchecker bytes-of [expected]
-  (checker [actual] (= (seq actual) (seq (.getBytes expected)))))
 
 (defn streams
   "Captures the first `n` streamed elements of c subject to a timeout of `timeout` ms"
@@ -22,40 +17,6 @@
                      (if (nil? v)
                        (conj out (if (= c port) ::channel-closed ::timeout))
                        (recur (conj out v)))))))))
-
-(defchecker refers-to [expected]
-  (checker [actual] (extended-= (deref actual) expected)))
-
-(defchecker eventually-refers-to [timeout expected]
-  (checker [actual]
-           (loop [t timeout]
-             (when (pos? t)
-               (if-let [result (extended-= (deref actual) expected)]
-                 result
-                 (do (Thread/sleep 200)
-                     (recur (- t 200))))))))
-
-(defchecker eventually-vrefers-to [timeout expected]
-  (checker [actual]
-           (loop [t timeout]
-             (when (pos? t)
-               (if-let [result (extended-= (.vDeref actual) expected)]
-                 result
-                 (do (Thread/sleep 200)
-                     (recur (- t 200))))))))
-
-(defchecker having-metadata [expected]
-  (every-checker (chatty-checker [actual] (extended-= actual expected))
-                 (chatty-checker [actual] (extended-= (meta actual) (meta expected)))))
-
-(defchecker as-ex-info
-  [expected]
-  (chatty-checker [actual]
-                  (extended-= [(ex-message actual) (ex-data actual) (ex-cause actual)] expected)))
-
-(defchecker eventually-streams [n timeout expected]
-  ;; The key to chatty checkers is to have the useful intermediate results be the evaluation of arguments to top-level expressions!
-  (chatty-checker [actual] (extended-= (streams n timeout actual) expected)))
 
 (defn- check-map
   [expected]
@@ -87,28 +48,6 @@
                                ((check-map base) actual))))))
 
 (def initial-stat? {:version -1 :aversion -1 :cversion -1})
-
-(defchecker stat? [expected]
-  ;; The key to chatty checkers is to have the useful intermediate results be the evaluation of arguments to top-level expressions!
-  (every-checker (some-checker (just {:version int?
-                                      :cversion int?
-                                      :aversion int?
-                                      :ctime (partial instance? java.time.Instant)
-                                      :mtime (partial instance? java.time.Instant)
-                                      :mzxid pos-int?
-                                      :czxid pos-int?
-                                      :pzxid pos-int?
-                                      :numChildren int?
-                                      :ephemeralOwner int?
-                                      :dataLength int?})
-                               (just {:version -1 :cversion -1 :aversion -1}))
-                 (contains expected)))
-
-(defchecker event? [expected]
-  ;; The key to chatty checkers is to have the useful intermediate results be the evaluation of arguments to top-level expressions!
-  (every-checker (contains {:type keyword?
-                            :path string?})
-                 (contains expected)))
 
 (defn stat-to-map
   ([^Stat stat]
