1d2ab86526a5967b426fbfb589513e9d9bb2e27a
Chris Hapgood <cch1@hapgood.com>
2022-05-19 11:56:00 -0400

    Stop defensively checking for presence of stat object
    It's the caller's responsibility.

--- src/zk/node.clj
+++ src/zk/node.clj
@@ -36,18 +36,17 @@
 (defn- stat-to-map
   ([^Stat stat]
    ;; https://zookeeper.apache.org/doc/trunk/zookeeperProgrammers.html#sc_timeInZk
-   (when stat
-     {:czxid (.getCzxid stat)
-      :mzxid (.getMzxid stat)
-      :pzxid (.getPzxid stat)
-      :ctime (Instant/ofEpochMilli (.getCtime stat))
-      :mtime (Instant/ofEpochMilli (.getMtime stat))
-      :version (.getVersion stat)
-      :cversion (.getCversion stat)
-      :aversion (.getAversion stat)
-      :ephemeralOwner (.getEphemeralOwner stat)
-      :dataLength (.getDataLength stat)
-      :numChildren (.getNumChildren stat)})))
+   {:czxid (.getCzxid stat)
+    :mzxid (.getMzxid stat)
+    :pzxid (.getPzxid stat)
+    :ctime (Instant/ofEpochMilli (.getCtime stat))
+    :mtime (Instant/ofEpochMilli (.getMtime stat))
+    :version (.getVersion stat)
+    :cversion (.getCversion stat)
+    :aversion (.getAversion stat)
+    :ephemeralOwner (.getEphemeralOwner stat)
+    :dataLength (.getDataLength stat)
+    :numChildren (.getNumChildren stat)}))
 
 (def ^:private default-stat {:version -1 :cversion -1 :aversion -1})
 
